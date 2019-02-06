7915ff981a9a13332121ecd9a024a6fe91582a3a
konrad <konrad@kola-entertainments.de>
2019-02-06 21:20:51 +0100

    todo

--- files/file.go
+++ files/file.go
@@ -52,6 +52,10 @@ type FileOptions struct {
 // object will be automatically filled depending on if it is a directory
 // or a file. If it's a video file, it will also detect any subtitles.
 func NewFileInfo(opts FileOptions) (*FileInfo, error) {
+
+	// This would be a good entrypoint for user shared files
+	// Somewhere in this file
+
 	if !opts.Checker.Check(opts.Path) {
 		return nil, os.ErrPermission
 	}
